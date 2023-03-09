539ab9f56f1e166449af95518ee58ca5fa51d481
Jerry Lin <jclin18@gmail.com>
2023-03-09 15:22:59 -0800

    Worked through Ch. 10

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,7 +6,10 @@ import (
 )
 
 func (app *application) logError(r *http.Request, err error) {
-	app.logger.Print(err)
+	app.logger.PrintError(err, map[string]string{
+		"request_method": r.Method,
+		"request_url":    r.URL.String(),
+	})
 }
 
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
@@ -27,8 +30,8 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
-    message := "the requested resource could not be found"
-    app.errorResponse(w, r, http.StatusNotFound, message)
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
