5bebfa113eb4f58da47ddc072703df2044b81626
Jerry Lin <jclin18@gmail.com>
2023-03-12 14:10:59 -0700

    Worked through Ch. 14

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -29,14 +29,24 @@ func (app *application) serve() error {
 
 		s := <-quit
 
-		app.logger.PrintInfo("shutting down server", map[string]string{
+		app.logger.PrintInfo("caught signal", map[string]string{
 			"signal": s.String(),
 		})
-
+		
 		ctx, cancel := context.WithTimeout(context.Background(), 20 * time.Second)
 		defer cancel()
 
-		shutdownError <- srv.Shutdown(ctx)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+
+		app.logger.PrintInfo("completing background tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
